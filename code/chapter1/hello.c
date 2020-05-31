ae6fd2aab522184a9edb0949f9db6b1703a42fcb
LiyangHao <liyanghao19890724@163.com>
2020-05-31 22:49:43 +0800

    Update hello.c

--- code/chapter1/hello.c
+++ code/chapter1/hello.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+// 预处理后生成hello.i文件
+// gcc -E hello.c -o hello.i
+
+// 编译后生成hello.s文件
+// gcc -S hello.i
+
+// 汇编后生成hello.o文件
+// gcc -c hello.c -o hello.o
+// 或者
+// gcc -c hello.i -o hello.o
+
+// 链接后生成hello
+// gcc -o hello hello.c
+// 或者
+// gcc -o hello hello.o
+
 
 int main()
 {
